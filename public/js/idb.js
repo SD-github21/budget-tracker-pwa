88d9a474bd19297c9c7b148a9ceb815970f671bb
SD-github21 <sapandonde.gmail.com>
2022-07-24 13:15:54 -0500

    created the indexedDB connection

---
+++ public/js/idb.js
@@ -0,0 +1,31 @@
+// Create a variable to hold db connection
+let db;
+// Establish a connection to IndexedDB database called 'budget-tracker' and set it to version 1
+const request = indexedDB.open('budget-tracker', 1);
+
+// This event will emit if the database version changes (nonexistent to version 1, v1 to v2, etc.)
+request.onupgradeneeded = function(event) {
+    // Save a reference to the database
+    const db = event.target.result
+    // Create an object store (table) called 'new_budget', set it to have an auto-incrementing primary key of sorts
+    db.createObjectStore('new_budget', { autoIncrement: true }); 
+};
+
+// Upon a successful db creation
+request.onsuccess = function(event) {
+    // When db is successfully created with its object store (from onupgradeneeded event above)
+    // or simply established a connection, save reference to db in global variable
+    db = event.target.result;
+
+    // Check if app is online, if yes run uploadBudget() function to send all local db data to API
+    if (navigator.onLine) {
+        // uploadBudget();
+    }
+};
+
+// If not successful db creation occurs
+request.onerror = function(event) {
+    // Log the error here
+    console.log(event.target.errorCode);
+};
+
