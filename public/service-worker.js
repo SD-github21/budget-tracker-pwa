bcec13856ef1b5fceeabd4dd760845f161d080bc
SD-github21 <sapandonde.gmail.com>
2022-07-30 10:42:29 -0500

    fixed syntax of files to cache

--- public/service-worker.js
+++ public/service-worker.js
@@ -2,9 +2,9 @@ const CACHE_NAME = 'budget-tracker-cache-v1';
 const DATA_CACHE_NAME = 'data-cache-v1';
 
 const FILES_TO_CACHE = [
-    '/',
-    '/index.html',
-    '/manifest.json',
+    './',
+    './index.html',
+    './manifest.json',
     './css/styles.css',
     './icons/icon-72x72.png',
     './icons/icon-96x96.png',
